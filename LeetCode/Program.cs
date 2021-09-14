bf92ebc930b8f4a7c28207fcd8b84205264fb34c
Erhan Kabuk <erhan.kabuk@bennusoft.com>
2021-09-14 12:52:46 +0300

    Binary Search

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -13,8 +13,8 @@ namespace BinarySearch
 
             /*Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
             You must write an algorithm with O(log n) runtime complexity.
-            Example 1:
 
+            Example 1:
             Input: nums = [-1,0,3,5,9,12], target = 9
             Output: 4
             Explanation: 9 exists in nums and its index is 4*/
@@ -26,7 +26,6 @@ namespace BinarySearch
             foreach (int item in nums)            
             Console.Write(item+ " ");            
             Console.WriteLine();
-
             Console.WriteLine(BinarySearch(nums, target));
 
             Console.ReadKey();
@@ -48,7 +47,6 @@ namespace BinarySearch
                     left = pivot + 1;
             }
             return -1;           
-
         }
     }
 }
