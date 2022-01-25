a967577feb6ccef8381b24012e5d22af9ee3cf0a
erhankabuk <erhankabuk@hotmail.com>
2022-01-26 00:05:35 +0300

    RegularExpression ValidAnagram

---
+++ RegularExpressionMatching/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace RegularExpressionMatching
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+            '.' Matches any single character.​​​​
+            '*' Matches zero or more of the preceding element.
+            The matching should cover the entire input string (not partial).
+
+ 
+
+            Example 1:
+
+            Input: s = "aa", p = "a"
+            Output: false
+            Explanation: "a" does not match the entire string "aa".
+            Example 2:
+
+            Input: s = "aa", p = "a*"
+            Output: true
+            Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+            Example 3:
+
+            Input: s = "ab", p = ".*"
+            Output: true
+            Explanation: ".*" means "zero or more (*) of any character (.)".
+            */
+
+            Console.WriteLine(IsMatch("aa", ".*"));
+            Console.ReadKey();
+        }
+
+        public static bool IsMatch(string s, string p)
+        {
+            char[] sArray = s.ToCharArray();
+            char[] pArray = p.ToCharArray();
+
+
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                if (pArray.Length<i)
+                {
+                    return false;
+                }
+                
+                
+                    if (sArray[i] != pArray[i])
+                    {
+                        if (pArray[i]== '.' && pArray[i + 1]=='*')
+                        {
+                            return true;
+                        }
+                        else if (pArray[i]=='*')
+                        {
+                        return true;
+                        }
+                        
+                    }
+                 
+
+
+
+                
+            }
+
+
+
+            return false;
+        }
+
+
+
+    }
+}
---
+++ ValidAnagram/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidAnagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Given two strings s and t, return true if t is an anagram of s, and false otherwise. 
+            An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original     letters    exactly once.          
+             
+            Example 1:            
+            Input: s = "anagram", t = "nagaram"
+            Output: true
+
+            Example 2:            
+            Input: s = "rat", t = "car"
+            Output: false
+
+            */
+            string s = "anagram";
+            string t = "nagaram";
+            Console.WriteLine(IsAnagram(s, t));
+            Console.ReadKey();
+        }
+
+
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < t.Length; i++) list.Add(t.Substring(i, 1));
+            
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (!list.Contains(s.Substring(i, 1)))return false;
+                
+                list.Remove(s.Substring(i, 1));
+            }
+
+            if (list.Count == 0) return true;
+
+            return false;
+
+        }
+    }
+}
