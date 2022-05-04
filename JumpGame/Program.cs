0b6687f4fd7269c5d7fdcc4cc1d9281cd499ebbb
Erhan Kabuk <erhankabuk@hotmail.com>
2022-05-04 17:13:26 +0300

    ValidParanthesis

---
+++ JumpGame/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace JumpGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+                Return true if you can reach the last index, or false otherwise.                 
+                
+                Example 1:
+                
+                Input: nums = [2,3,1,1,4]
+                Output: true
+                Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+                Example 2:
+                
+                Input: nums = [3,2,1,0,4]
+                Output: false
+                Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+            */
+
+            int[] input1 = new int[] { 2, 3, 1, 1, 4 };//true
+            int[] input2 = new int[] { 3, 2, 1, 0, 4 };//false
+            int[] input3 = new int[] {  0, 1 };//false
+
+            Console.WriteLine(CanJump(input3));
+            Console.ReadKey();
+        }
+
+        public static bool CanJump(int[] nums)
+        {
+
+            int length = nums.Length;
+
+            for(int i =0; i<length; i++)
+            {
+                if (nums[i] <= length - 2) return false;
+                if (i == length - 1) return true;
+                
+                else if (nums[i] <= length - 1)
+                {
+                    i = nums[i] - 1;
+                    length = nums[i] + 1;
+                }
+              
+
+
+
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ValidParenthesis/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ValidParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+             * 
+            An input string is valid if:
+
+            Open brackets must be closed by the same type of brackets.
+            Open brackets must be closed in the correct order. 
+
+            Example 1:
+
+            Input: s = "()"
+            Output: true
+            Example 2:
+
+            Input: s = "()[]{}"
+            Output: true
+            Example 3:
+
+            Input: s = "(]"
+            Output: false
+            */
+            //string input = "{[]}()";
+            string input = "{[]}())))))";
+            Console.WriteLine(IsValid(input));
+        }
+        public static bool IsValid(string input)
+        {
+            Stack<char> sign = new Stack<char>();
+            char[] arr = input.ToCharArray();
+            foreach (var item in arr)
+                if (item == '(')
+                    sign.Push(')');
+                else if (item == '[')
+                    sign.Push(']');
+                else if (item == '{')
+                    sign.Push('}');
+                else if (sign.Count == 0 || sign.Pop() != item)
+                    return false;
+
+            return sign.Count == 0;
+        }
+    }
+}
