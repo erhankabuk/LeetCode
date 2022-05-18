36f253aa1a40484bef5224818243c020c7371d30
Erhan Kabuk <erhankabuk@hotmail.com>
2022-05-18 12:41:21 +0300

    PrimeChecker

--- PrimeChecker/Program.cs
+++ PrimeChecker/Program.cs
@@ -6,9 +6,10 @@ namespace PrimeChecker
     class Program
     {
         //Have the function PrimeChecker(num) take num and return 1 if any arrangement of num comes out to be a prime number, otherwise return 0. For example: if num is 910, the output should be 1 because 910 can be arranged into 109 or 019, both of which are primes.
+
         public static void Main(string[] args)
         {
-           
+        //Convert for case   
             PrintResult(
                   Permute(new int[] { 1, 2, 3 })
               );
